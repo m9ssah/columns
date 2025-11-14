070411713977ee76c330bae4991c6cb376d03c3f
massah <arafeh.massah2007@gmail.com>
2025-11-14 01:24:44 -0500

    constants/data

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # **Columns**
 *NOTE: dont forget to remove bitmap_display.asm and keyboard when done*
-## Complete Remake of Columns (tetris) using *Assembly*
+## Complete Remake of Columns (retro arcade game) using *Assembly*
 ### Features:
-- 
+- Patterned gems
+- Sound FX
--- columns.asm
+++ columns.asm
@@ -8,27 +8,61 @@
 # creation, and will indicate otherwise when it is not.
 #
 ######################## Bitmap Display Configuration ########################
-# - Unit width in pixels:       TODO
-# - Unit height in pixels:      TODO
-# - Display width in pixels:    TODO
-# - Display height in pixels:   TODO
+# - Unit width in pixels: 8
+# - Unit height in pixels: 8
+# - Display width in pixels: 256
+# - Display height in pixels: 256
 # - Base Address for Display:   0x10008000 ($gp)
 ##############################################################################
-
     .data
 ##############################################################################
 # Immutable Data
 ##############################################################################
-# The address of the bitmap display. Don't forget to connect it!
-ADDR_DSPL:
-    .word 0x10008000
-# The address of the keyboard. Don't forget to connect it!
-ADDR_KBRD:
-    .word 0xffff0000
+# Address of the bitmap display
+    ADDR_DSPL:  .word 0x10008000
+# Address of the keyboard
+    ADDR_KBRD:  .word 0xffff0000
+    
+# Colors:
+    black: 		.word 0x00000000        # black, misc
+    blue:       .word 0x000E2258        # penn blue/navy color, bg
+    indigo:     .word 0x00432AFF        # indigo/purple color, gem
+    violet:     .word 0x00FF76E6        # violet/pink, gen
+    mustard:    .word 0x00FFD767        # mustard/yellow, gem
+    green:      .word 0x0086FA9F        # light green color, gem
+    cyan:       .word 0x009EE6E6        # cyan/celeste color, bg
+    white:      .word 0x00FFFFFF        # white color, misc
+    
+# Game Scene Constants:
+	grid_w:     .word 13           # game field width
+	grid_h:     .word 24           # game field height
+	
+	unit_size:  .word 8            # gem size
+    dspl_width: .word 256
+	dspl_height:.word 256
+
+	t_border:   .word 3             # top  border
+    
+    
+# Extra features (milestone 4,5):
+    gravity_time:   .word 0     # TODO change when implementing gravity
+    
+    music_timer:    .word 0     # TODO change when implementing soundfx/theme music
+
 
 ##############################################################################
 # Mutable Data
 ##############################################################################
+
+# Active falling col:
+    curr_x:         .word 0
+    curr_y:         .word 0
+    curr_colors:    
+    
+# Game state:
+    score:          .word 0
+    jewels:         .word 0
+
 
 ##############################################################################
 # Code
@@ -36,10 +70,11 @@ ADDR_KBRD:
 	.text
 	.globl main
 
-    # Run the game.
-main:
-    # Initialize the game
-
+    # Run the game
+    
+main:       # Initialize the game
+    
+    
 game_loop:
     # 1a. Check if key has been pressed
     # 1b. Check which key has been pressed
